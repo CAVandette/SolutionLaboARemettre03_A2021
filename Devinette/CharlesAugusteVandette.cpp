3ae16ac295df5930d6543a45a3fd6f27d50652f7
Charles-Auguste Vandette <2077700@cstj.qc.ca>
2021-09-14 20:50:01 -0400

    Ajoutez des fichiers projet.

---
+++ Devinette/CharlesAugusteVandette.cpp
@@ -0,0 +1,19 @@
+// But : 
+// Auteur : Charles-Auguste Vandette
+// Date : 2021-09-14
+
+#include<iostream>
+
+using namespace std;
+
+void main()
+{
+   setlocale(LC_ALL, "");
+
+   // Déclaration et initialisation des constantes
+
+
+   // Déclaration des variables
+
+   system("pause");
+}
